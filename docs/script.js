ebe4ff94c046491ce07154a676bafa13841f4e8c
dreyaq <dreyaq@yandex.ru>
2023-10-14 19:56:01 +0300

    pract_6

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,11 @@
 const headerBurger = document.querySelector('.header_burger');
 const headerMenu = document.querySelector('.header_menu');
+const body = document.querySelector('body');
 
 const burgerClickHandler = () => {
     headerBurger.classList.toggle("active");
-    headerMenu.classList.toggle("active")
+    headerMenu.classList.toggle("active");
+    body.classList.toggle('lock');
 };
 
 headerBurger.addEventListener("click", burgerClickHandler);
