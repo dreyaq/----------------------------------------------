ee48ad95c514ef5b31a24e5751d8bbfde466b3b2
dreyaq <dreyaq@yandex.ru>
2023-11-25 17:25:38 +0300

    all_without_works

--- docs/script.js
+++ docs/script.js
@@ -9,3 +9,34 @@ const burgerClickHandler = () => {
 };
 
 headerBurger.addEventListener("click", burgerClickHandler);
+
+
+document.querySelector('#video_1').addEventListener('mouseenter', function() {
+    document.querySelector('#h2_1').classList.add('hovered');
+  });
+  
+  document.querySelector('#video_1').addEventListener('mouseleave', function() {
+    document.querySelector('#h2_1').classList.remove('hovered');
+  });
+
+  document.querySelector('#video_2').addEventListener('mouseenter', function() {
+    document.querySelector('#h2_2').classList.add('hovered');
+  });
+  
+  document.querySelector('#video_2').addEventListener('mouseleave', function() {
+    document.querySelector('#h2_2').classList.remove('hovered');
+  });
+  document.querySelector('#video_3').addEventListener('mouseenter', function() {
+    document.querySelector('#h2_3').classList.add('hovered');
+  });
+  
+  document.querySelector('#video_3').addEventListener('mouseleave', function() {
+    document.querySelector('#h2_3').classList.remove('hovered');
+  });
+  document.querySelector('#video_4').addEventListener('mouseenter', function() {
+    document.querySelector('#h2_4').classList.add('hovered');
+  });
+  
+  document.querySelector('#video_4').addEventListener('mouseleave', function() {
+    document.querySelector('#h2_4').classList.remove('hovered');
+  });
